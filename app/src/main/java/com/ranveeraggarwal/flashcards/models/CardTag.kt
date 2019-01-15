cd569c9a71a71c8f0cfb03b6d5d6aa9f2852191a
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2019-01-15 10:44:08 +0530

    FlashCard and Tag models

---
+++ app/src/main/java/com/ranveeraggarwal/flashcards/models/CardTag.kt
@@ -0,0 +1,15 @@
+package com.ranveeraggarwal.flashcards.models
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.Index
+import android.support.annotation.NonNull
+
+@Entity(tableName = "card_tag",
+    foreignKeys = [ForeignKey(entity = FlashCard::class, parentColumns=["cid"],
+        childColumns = ["cardId"], onDelete = ForeignKey.CASCADE)],
+    indices = [Index(value = ["cardId", "tagName"], unique = true)],
+    primaryKeys = ["cardId", "tagName"])
+data class CardTag(@ColumnInfo(name = "cardId") @NonNull val cardId: Long,
+                   @ColumnInfo(name = "tagName") @NonNull val tagName: String)
--- app/src/main/java/com/ranveeraggarwal/flashcards/models/FlashCard.kt
+++ app/src/main/java/com/ranveeraggarwal/flashcards/models/FlashCard.kt
@@ -1,3 +1,13 @@
 package com.ranveeraggarwal.flashcards.models
 
-data class FlashCard (val id: Long, val question: String, val answer: String)
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Index
+import android.arch.persistence.room.PrimaryKey
+import android.support.annotation.NonNull
+
+@Entity(tableName = "flashCard",
+    indices = [Index(value = ["cid"], unique = true)])
+data class FlashCard (@PrimaryKey(autoGenerate = true) @ColumnInfo(name = "cid") val id: Long,
+                      @ColumnInfo(name = "question") @NonNull val question: String,
+                      @ColumnInfo(name = "answer") @NonNull val answer: String)
