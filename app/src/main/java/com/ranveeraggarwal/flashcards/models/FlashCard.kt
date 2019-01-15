68ac74d0a9dec74da188c469c24cfbbfa693b304
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2019-01-15 15:25:32 +0530

    Removing auto-increment from cid - the values will be synced from the server DB

--- app/src/main/java/com/ranveeraggarwal/flashcards/models/FlashCard.kt
+++ app/src/main/java/com/ranveeraggarwal/flashcards/models/FlashCard.kt
@@ -7,7 +7,8 @@ import android.arch.persistence.room.PrimaryKey
 import android.support.annotation.NonNull
 
 @Entity(tableName = "flashCard",
-    indices = [Index(value = ["cid"], unique = true)])
-data class FlashCard (@PrimaryKey(autoGenerate = true) @ColumnInfo(name = "cid") val id: Long,
+    indices = [Index(value = ["cid"], unique = true)],
+    primaryKeys = ["cid"])
+data class FlashCard (@ColumnInfo(name = "cid") val id: Long,
                       @ColumnInfo(name = "question") @NonNull val question: String,
                       @ColumnInfo(name = "answer") @NonNull val answer: String)
